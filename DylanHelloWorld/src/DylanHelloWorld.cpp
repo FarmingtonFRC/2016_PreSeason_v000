3192f3b3b6879ed8bd7f7bdae178fcde37044c2a
Dylan Barton <dylan.barton@mail.com>
2015-11-17 22:00:41 -0600

    Please Work!

---
+++ DylanHelloWorld/src/DylanHelloWorld.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : DylanHelloWorld.cpp
+// Author      : Dylan Barton
+// Version     :
+// Copyright   : FRC Team #2987
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World, Dylan!!!" << endl; // prints !!!Hello World, Dylan!!!
+	return 0;
+}

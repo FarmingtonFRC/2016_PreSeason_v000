a07c4cd2b8bb3417648290edb567e65e553b936e
cudy789 <corey.knutson789@gmail.com>
2015-11-17 20:25:11 -0600

    Corey Adding Helloworld
    ^^^^

---
+++ CoreyHelloWorld/src/CoreyHelloWorld.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : CoreyHelloWorld.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Corey got hello world to work!!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
